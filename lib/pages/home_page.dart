b3a54e41d9562d15efa7581972cfbe32351d7fe2
usman619 <usmanshahab191@gmail.com>
2024-11-11 18:52:28 +0500

    Add message clearing and Markdown support in chat

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gemini/bloc/chat_bloc.dart';
 import 'package:flutter_gemini/models/chat_message_model.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 
@@ -15,6 +17,35 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final ChatBloc chatBloc = ChatBloc();
   TextEditingController textEditingController = TextEditingController();
+  bool clear_messages = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _clearMessages(List<ChatMessageModel> messages) {
+    setState(() {
+      clear_messages = true;
+    });
+    // Fluttertoast.showToast(
+    //   msg: 'Chat cleared!',
+    //   toastLength: Toast.LENGTH_SHORT,
+    //   gravity: ToastGravity.BOTTOM,
+    //   backgroundColor: Colors.red,
+    //   textColor: Colors.white,
+    // );
+    messages.clear();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("Chat Cleared!",
+            style: GoogleFonts.firaCode(
+              color: Colors.white,
+            )),
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,14 +89,29 @@ class _HomePageState extends State<HomePage> {
                           crossAxisAlignment: CrossAxisAlignment.end,
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            const Text("Flutter Gemini",
-                                style: TextStyle(fontSize: 24)),
-                            IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                Icons.image_search,
-                                size: 24,
-                              ),
+                            const Text(
+                              "Flutter Gemini",
+                              style: TextStyle(fontSize: 24),
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () => _clearMessages(messages),
+                                  icon: const Icon(
+                                    Icons.clear_rounded,
+                                    size: 24,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                // IconButton(
+                                //   onPressed: () {},
+                                //   icon: const Icon(
+                                //     Icons.image_search,
+                                //     size: 24,
+                                //     color: Colors.white,
+                                //   ),
+                                // )
+                              ],
                             )
                           ],
                         ),
@@ -99,13 +145,29 @@ class _HomePageState extends State<HomePage> {
                                     endIndent: 100,
                                     color: Colors.white,
                                   ),
-                                  Text(
-                                    messages[index].parts.first.text,
-                                    style: GoogleFonts.firaCode(
-                                      fontSize: 18,
-                                      color: Colors.white,
-                                    ),
-                                  ),
+                                  messages[index].role == "user"
+                                      ? Text(
+                                          messages[index].parts.first.text,
+                                          style: GoogleFonts.firaCode(
+                                            fontSize: 18,
+                                            color: Colors.white,
+                                          ),
+                                        )
+                                      : MarkdownBody(
+                                          data:
+                                              messages[index].parts.first.text,
+                                          styleSheet: MarkdownStyleSheet(
+                                            p: GoogleFonts.firaCode(
+                                              fontSize: 18,
+                                              color: Colors.white,
+                                            ),
+                                            code: GoogleFonts.firaCode(
+                                              fontSize: 16,
+                                              color: Colors.grey[300],
+                                              backgroundColor: Colors.grey[800],
+                                            ),
+                                          ),
+                                        ),
                                 ],
                               ),
                             );
@@ -126,29 +188,34 @@ class _HomePageState extends State<HomePage> {
                         child: Row(
                           children: [
                             Expanded(
-                              child: TextField(
-                                controller: textEditingController,
-                                style: GoogleFonts.firaCode(
-                                  fontSize: 14,
-                                  color: Colors.black,
-                                ),
-                                cursorColor: Colors.black,
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(100),
+                              child: SingleChildScrollView(
+                                scrollDirection: Axis.vertical,
+                                child: TextField(
+                                  controller: textEditingController,
+                                  style: GoogleFonts.firaCode(
+                                    fontSize: 16,
+                                    color: Colors.black,
                                   ),
-                                  hintText: "Ask Gemini anything...",
-                                  hintStyle: GoogleFonts.firaCode(
-                                    fontSize: 14,
-                                    color: Colors.grey[300],
+                                  cursorColor: Colors.black,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(100),
+                                    ),
+                                    hintText: "Ask Gemini anything...",
+                                    hintStyle: GoogleFonts.firaCode(
+                                      fontSize: 16,
+                                      color: Colors.grey[400],
+                                    ),
+                                    fillColor: Colors.white,
+                                    filled: true,
+                                    focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                      borderSide:
+                                          const BorderSide(color: Colors.black),
+                                    ),
                                   ),
-                                  fillColor: Colors.white,
-                                  filled: true,
-                                  focusedBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(100),
-                                    borderSide:
-                                        const BorderSide(color: Colors.black),
-                                  ),
+                                  minLines: 1,
+                                  maxLines: 5,
                                 ),
                               ),
                             ),
